9336b624cd6d2827b7d40113804e70bafbb3d8e3
이상윤 <sylagape1231@gmail.com>
2025-08-24 12:26:40 +0900

    Add: 기본 파일 생성

---
+++ script.js
@@ -0,0 +1,14 @@
+// 할일 추가
+function addTodo(text) {}
+
+// 할일 삭제
+function deleteTodo(id) {}
+
+// 할일 수정
+function modifyTodo(id, text) {}
+
+// 할일 완료
+function toggleTodo(id) {}
+
+// 현재 할일 목록 출력
+function printTodoList() {}
